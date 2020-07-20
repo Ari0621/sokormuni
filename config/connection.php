4507cd58cf9c7ce8ad6e70c5e05efa7a123f7e8d
Ari0621 <arimalang21@gmail.com>
2020-07-20 15:26:43 +0700

    buat  database

--- config/connection.php
+++ config/connection.php
@@ -8,3 +8,10 @@ if ($koneksi->connect_error) {
     die("Connection Failed: " . $koneksi->connect_error);
   }
   echo "Connection Sukses";
+
+  $sql = "CREATE DATABASE sokormuni";
+if ($koneksi->query($sql) === TRUE) {
+    echo "Database berhasil di buat";
+}else{
+    echo "database error " . $koneksi->error;
+}
